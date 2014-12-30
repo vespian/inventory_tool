f4f529aca5a37a19c3445ec7fc572ece08ba4293
Pawel Rozlach <pawel.rozlach@gmail.com>
2014-12-30 18:20:58 +0100

    Add missing preamble for wrapper script
    Change-Id: I26d002ca739544bc3d431a6cdb6b441bd33deb5a

--- examples/hosts-production.py
+++ examples/hosts-production.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-# (c) 2014 Brainly.com, Pawel Rozlach <pawel.rozlach@brainly.com>
-
-# This script is intended to only find "where it is" and invoke the inventory
-# tool with correct inventory path basing on it's name.
+# Copyright (c) 2014 Pawel Rozlach, Brainly.com sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
 import os.path as op
 import sys
