a3950068e2dff2509c0859a3b5d5150ebf842543
Vespian <vespian@wp.pl>
2015-01-04 20:17:18 +0100

    "validators" module unittests
    Change-Id: I71c923afa8c27cd0c547ab4509c06747e1c6c9bc

--- test/moduletests/inventory_tool/test_validators.py
+++ test/moduletests/inventory_tool/test_validators.py
@@ -198,3 +198,99 @@ class TestGetName(unittest.TestCase):
         name_str = "this is a bad name"
         with self.assertRaises(argparse.ArgumentTypeError):
             v.get_name(name_str)
+
+
+class TestGetKeyVal(unittest.TestCase):
+    def setUp(self):
+        patcher = mock.patch('inventory_tool.validators.KeyWordValidator')
+        self.KeyWordValidatorMock = patcher.start()
+        self.addCleanup(patcher.stop)
+        self.KeyWordValidatorMock.is_integer_keyword.return_value = False
+        self.KeyWordValidatorMock.is_ipnetwork_keyword.return_value = False
+        self.KeyWordValidatorMock.is_ipaddress_keyword.return_value = False
+        self.KeyWordValidatorMock.is_connection_keyword.return_value = False
+
+    def test_non_keyval_arg(self):
+        name_str = "this is:a bad keyval"
+        with self.assertRaises(argparse.ArgumentTypeError):
+            v.get_keyval(name_str)
+
+    def test_plain_keyval(self):
+        keyval = {"key": "integer_key", "val": "some_val"}
+        ret = v.get_keyval("{0}:{1}".format(keyval["key"], keyval['val']))
+        self.assertEqual(ret, keyval)
+        self.KeyWordValidatorMock.is_integer_keyword.assert_called_once_with(keyval['key'])
+
+    def test_good_integer_keyval(self):
+        self.KeyWordValidatorMock.is_integer_keyword.return_value = True
+        keyval = {"key": "integer_key", "val": 1234}
+        ret = v.get_keyval("{0}:{1}".format(keyval["key"], keyval['val']))
+        self.assertEqual(ret, keyval)
+        self.KeyWordValidatorMock.is_integer_keyword.assert_called_once_with(keyval['key'])
+
+    def test_bad_integer_keyval(self):
+        self.KeyWordValidatorMock.is_integer_keyword.return_value = True
+        keyval = {"key": "integer_key", "val": 'not_an_int'}
+        with self.assertRaises(argparse.ArgumentTypeError):
+            v.get_keyval("{0}:{1}".format(keyval["key"], keyval['val']))
+
+    @mock.patch('inventory_tool.validators.get_ipaddr')
+    def test_good_ipaddress_keyval(self, GetIpMock):
+        key = "ipaddress_key"
+        val = "1.2.3.4"
+        self.KeyWordValidatorMock.is_ipaddress_keyword.return_value = True
+        GetIpMock.return_value = ip_address(val)
+        ret = v.get_keyval("{0}:{1}".format(key, val))
+        self.assertEqual(ret, {"key": key, "val": GetIpMock.return_value})
+        self.KeyWordValidatorMock.is_ipaddress_keyword.assert_called_once_with(key)
+
+    @mock.patch('inventory_tool.validators.get_ipaddr')
+    def test_bad_ipaddress_keyval(self, GetIpMock):
+        self.KeyWordValidatorMock.is_ipaddress_keyword.return_value = True
+
+        def raise_exception(*unused_l, **unused_kw):
+            raise argparse.ArgumentTypeError
+
+        GetIpMock.side_effect = raise_exception
+        keyval = {"key": "ipaddress_key", "val": 'not_an_ip'}
+        with self.assertRaises(argparse.ArgumentTypeError):
+            v.get_keyval("{0}:{1}".format(keyval["key"], keyval['val']))
+
+    @mock.patch('inventory_tool.validators.get_ipaddr')
+    def test_autoassigned_ipaddress_keyval(self, GetIpMock):
+        key = "ipaddress_key"
+        self.KeyWordValidatorMock.is_ipaddress_keyword.return_value = True
+        ret = v.get_keyval(key)
+        self.assertEqual(ret, {"key": key, "val": None})
+        self.KeyWordValidatorMock.is_ipaddress_keyword.assert_called_once_with(key)
+
+    def test_good_ipnetwork_keyval(self):
+        self.KeyWordValidatorMock.is_ipnetwork_keyword.return_value = True
+        key = "ipnetwork_key"
+        val = "1.2.3.0/24"
+        ret = v.get_keyval("{0}:{1}".format(key, val))
+        self.assertEqual(ret, {"key": key, "val": ip_network(val)})
+        self.KeyWordValidatorMock.is_ipnetwork_keyword.assert_called_once_with(key)
+
+    def test_bad_ipnetwork_keyval(self):
+        self.KeyWordValidatorMock.is_ipnetwork_keyword.return_value = True
+        key = "ipnetwork_key"
+        val = "not-a-network"
+        with self.assertRaises(argparse.ArgumentTypeError):
+            v.get_keyval("{0}:{1}".format(key, val))
+
+    def test_good_connection_keyval(self):
+        self.KeyWordValidatorMock.is_connection_keyword.return_value = True
+        key = "ansible_connection"
+        val = "ssh"
+        ret = v.get_keyval("{0}:{1}".format(key, val))
+        self.assertEqual(ret, {"key": key, "val": val})
+        self.KeyWordValidatorMock.is_connection_keyword.assert_called_once_with(val)
+
+    def test_bad_connection_keyval(self):
+        self.KeyWordValidatorMock.is_connection_keyword.return_value = False
+        key = "ansible_connection"
+        val = "not-ssh"
+        with self.assertRaises(argparse.ArgumentTypeError):
+            v.get_keyval("{0}:{1}".format(key, val))
+        self.KeyWordValidatorMock.is_connection_keyword.assert_called_once_with(val)
